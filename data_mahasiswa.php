6a976612e41356fb1c9a792fd491d1d924376b24
SaidAkbar <154867993+saidakbaraja@users.noreply.github.com>
2024-02-19 15:41:09 +0700

    Update data_mahasiswa.php

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -1,6 +1,7 @@
 <?php
-echo "<h1>Data Mahasiswa</h1>";
+echo "<h1>Data Mahasiswa</h1> <br>";
 echo "NIM : 22110057 <br>";
-echo "Nama Lengkap : SaidAkbar";
-echo "Kelas : 22 Mi A";
+echo "Nama Lengkap : SaidAkbar <br>";
+echo "Kelas : 22 MI-A <br>";
+echo "Semester Genap";
 ?>
